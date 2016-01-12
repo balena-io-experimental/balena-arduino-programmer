7416175cf0ba611bf1cab76d2f1d2ca44d4e0789
gaudi <craig@resin.io>
2016-01-12 15:01:30 +0000

    change delay

--- src/blink/blink.ino
+++ src/blink/blink.ino
@@ -23,7 +23,7 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
+  delay(5000);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+  delay(5000);              // wait for a second
 }
